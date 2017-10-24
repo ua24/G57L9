c08e25e56accff4b729cc753a059ec0acac875c5
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2017-10-24 20:43:43 +0300

    tableView works fine

--- G57L9/ViewController.swift
+++ G57L9/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+	@IBOutlet weak var tableView: UITableView!
+	var array = "Created by Ivan Vasilevich on 10/24/17.".components(separatedBy: " ")
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		tableView.dataSource = self
+		tableView.delegate = self
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// MARK: - TableViewDataSource
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return array.count
 	}
-
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = array[indexPath.row]
+		cell.detailTextLabel?.text = (indexPath.row + 1).description
+		let odd = indexPath.row % 2 == 0
+		cell.imageView?.image = UIImage(named: odd ? "domo" : "ddasdadsdasd")
+		return cell
+	}
+	
+	// MARK: - TableViewDelegate
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(indexPath)
+	}
 
 }
 
